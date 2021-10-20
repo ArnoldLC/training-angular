f5a4218f246d675206e1a8e236d6249c92a6ad33
Arnold <arnold426499@gmail.com>
2021-10-20 18:35:07 -0500

    Formularios reactivos con FormBuilder y Validators

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -8,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    nombre: [ 'RTX 4080ti', [ Validators.required, Validators.minLength(3) ] ],
+    precio: [ '0', [ Validators.required, Validators.minLength(0) ] ],
+    existencias: [ '0', [ Validators.required, Validators.minLength(0) ] ]
+  })
 
-  ngOnInit(): void {
+  constructor( private fb: FormBuilder ) {
+
+  }
+
+  ngOnInit() {
+    this.miFormulario.reset({
+      nombre: 'RTX 4080ti',
+      precio: 3000
+    })
+  }
+
+  campoEsValido( campo: string ) {
+    return this.miFormulario.controls[campo].errors &&
+           this.miFormulario.controls[campo].touched
+  }
+
+  guardar() {
+    if( this.miFormulario.invalid ) {
+      this.miFormulario.markAllAsTouched()
+      return
+    }
+
+    this.miFormulario.reset()
   }
 
 }
--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -6,11 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
+
 export class SwitchesComponent implements OnInit {
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    genero: [ 'M', Validators.required ],
+    notificaciones: [ true, Validators.required ],
+    terminos: [ false, Validators.requiredTrue ]
+  })
 
-  ngOnInit(): void {
+  persona = {
+    genero: 'F',
+    notificaciones: true
+  }
+
+  constructor( private fb: FormBuilder ) {}
+
+  ngOnInit() {
+    this.miFormulario.reset({ 
+      ...this.persona, 
+      terminos: false 
+    })
+
+    this.miFormulario.valueChanges.subscribe( ({ terminos, ...restoDeArgumentos }) => {
+      // delete form.terminos
+      this.persona = restoDeArgumentos
+    })
+  }
+
+  guardar() {
+    const formValue = { ...this.miFormulario.value }
+    delete formValue.terminos
+
+    this.persona = formValue
   }
 
 }
